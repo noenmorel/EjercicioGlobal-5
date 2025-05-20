b7b84f67127a9559f40f2bb3bef33f5a1f0c8ca8
noenmorel <noenmorel@gmail.com>
2025-05-20 17:36:53 -0300

    ejercicios 0 y 1

--- src/test/java/com/bootcampW22/EjercicioGlobal/controller/VehicleControllerIntegrTest.java
+++ src/test/java/com/bootcampW22/EjercicioGlobal/controller/VehicleControllerIntegrTest.java
@@ -23,7 +23,42 @@ import java.io.ObjectInputFilter.Status;
 public class VehicleControllerIntegrTest {
     @Autowired
     MockMvc mockMvc;
+    //ejercicio 0
+    @Test
+    public void getVehicles_okTEst()throws Exception{
+        mockMvc.perform(get("/vehicles"))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].brand").value("Pontiac"));
+    }
 
+    @Test
+    public void getVehicles_noOkTEst()throws Exception{
+        mockMvc.perform(get("/vehicles"))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        //.andExpect(status().isNotFound())
+        //.andExpect(jsonPath("$[0].brand").value("Pontiac"))
+        ;
+    }
+
+    //ejerccio 1
+    @Test
+    public void getVehiclesByColorAndYear_okTest( ) throws Exception{
+        mockMvc.perform(get("/vehicles/color/{color}/year/{year}", "Mauv", 1986))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].color").value("Mauv"));
+    }
+
+    @Test
+    public void getVehiclesByColorAndYear_noOkTest( ) throws Exception{
+        mockMvc.perform(get("/vehicles/color/{color}/year/{year}", "Mauvvvvvv", 1986))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$.message").value("No se encontraron vehículos con esos criterios."));
+    }
+
+    //ejercicio 2
     @Test
     public void getVehiclesByColorAndRangeOfYear_okTest() throws Exception {
         mockMvc.perform(get("/vehicles/brand/{brand}/between/{start_year}/{end_year}","Ford",1997,2020))
--- src/test/java/com/bootcampW22/EjercicioGlobal/service/VehicleServiceTest.java
+++ src/test/java/com/bootcampW22/EjercicioGlobal/service/VehicleServiceTest.java
@@ -30,7 +30,70 @@ public class VehicleServiceTest {
 
     @InjectMocks
     VehicleServiceImpl vehicleServiceImpl;
+    
+    //ejercicio 0
+    @Test
+    public void searchAllVehicles_okTest(){
+        //arrange
+        List<Vehicle> listaRepo = CustomFactory.get0Vehicle(1L);
+        List<VehicleDto> listaEsperada = CustomFactory.get0VehicleDto(1L);
 
+        //act
+        when(vehicleRepositoryImpl.findAll()).thenReturn(listaRepo);
+        List<VehicleDto> respDto = vehicleServiceImpl.searchAllVehicles();
+
+        //assert
+        assertEquals(listaEsperada, respDto);
+        assertEquals(2, respDto.size());
+        verify(vehicleRepositoryImpl).findAll();//Verifica que el método findAll() haya sido llamado en el mock del repositorio.
+        verifyNoMoreInteractions(vehicleRepositoryImpl);//Asegura que no se haya llamado ningún otro método del repositorio aparte de findAll().
+    }
+
+    @Test
+    public void searchAllVehicles_noOkTest(){
+        //arrange
+        //List<Vehicle> listaRepo = CustomFactory.get0Vehicle(1L);
+        //List<VehicleDto> listaEsperada = CustomFactory.get0VehicleDto(1L);
+
+        //act
+        when(vehicleRepositoryImpl.findAll()).thenReturn(new ArrayList<>());
+        NotFoundException respDto = assertThrows(NotFoundException.class, ()-> vehicleServiceImpl.searchAllVehicles());
+
+        //assert
+        assertEquals("No se encontró ningun auto en el sistema.", respDto.getMessage());
+   }
+
+    //ejercicio 1
+   @Test
+   public void searchVehiclesByYearAndColor_oktest(){
+    //arrange
+    List<VehicleDto> esperada = CustomFactory.get1VehicleDtoEqualYearAndColor(1L, "red", 2020);
+    List<Vehicle> repo = CustomFactory.get1ListVehicleEqualYearAndColor(1L, "red", 2020);
+    
+    //act
+    when(vehicleRepositoryImpl.findVehiclesByYearAndColor("red", 2020)).thenReturn(repo);
+    List<VehicleDto> resp = vehicleServiceImpl.searchVehiclesByYearAndColor("red", 2020);
+
+    //assert
+    assertEquals(esperada, resp);
+    verify(vehicleRepositoryImpl).findVehiclesByYearAndColor("red",2020);
+    verifyNoMoreInteractions(vehicleRepositoryImpl);
+   }
+
+      @Test
+   public void searchVehiclesByYearAndColor_noOktest(){
+    //arrange
+    //List<VehicleDto> esperada = CustomFactory.get1VehicleDtoEqualYearAndColor(1L, "red", 2020);
+    //ist<Vehicle> repo = CustomFactory.get1ListVehicleEqualYearAndColor(1L, "red", 2020);
+    
+    //act
+    when(vehicleRepositoryImpl.findVehiclesByYearAndColor("red", 2020)).thenReturn(new ArrayList<>());
+   NotFoundException resp = assertThrows(NotFoundException.class, () -> vehicleServiceImpl.searchVehiclesByYearAndColor("red", 2020));
+
+    //assert
+    assertEquals("No se encontraron vehículos con esos criterios.", resp.getMessage());
+   }
+    //ejercicio 2
     @Test
     public void searchVehiclesByBrandAndRangeOfYear_okTest(){
         //Arrange
